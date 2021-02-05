0c8aac320c85ae92a69fc1d9843989e64e62b10b
luca-de-martini <10565376@polimi.it>
2021-02-05 13:13:44 +0100

    Improve time estimate for tickets

--- ITD/backend/tests/account_api_test.rs
+++ ITD/backend/tests/account_api_test.rs
@@ -8,8 +8,29 @@ use rand::{Rng, thread_rng};
 use actix_web::test;
 
 #[actix_rt::test]
-async fn register_test() {
-    let mut app = setup_app!();
+async fn account_api_test() {
+    let mut app = {
+        use std::env;
+        use clup::api;
+        dotenv::dotenv().ok();
+        let conn_url = env::var("DATABASE_URL").expect("DATABASE_URL environment variable must be set");
+        let db_pool = clup::setup_db(&conn_url).await;
+
+        let redis_url = env::var("REDIS_URL").expect("REDIS_URL environment variable must be set");
+        let session_key = env::var("SESSION_KEY").expect("SESSION_KEY environment variable must be set");
+        let key = hex::decode(session_key).expect("Invalid SESSION_KEY format. Expected hex");
+
+        actix_web::test::init_service(actix_web::App::new()
+            .data(db_pool.clone())
+            .wrap(actix_redis::RedisSession::new(&redis_url, &key))
+            .wrap(actix_web::middleware::Logger::default())
+            .configure(api::account::endpoints)
+            .configure(api::ticket::endpoints)
+            .configure(api::shop::endpoints)
+            .service(actix_web::web::scope("/staff").configure(api::staff::endpoints))
+            .service(actix_web::web::scope("/dev").configure(api::dev::endpoints))
+        ).await
+    };
     
     let (usr, pass): (u32, u32) = (thread_rng().gen(), thread_rng().gen());
     let (email, password) = (format!("{:x}@test.com", usr), format!("{:x}", pass));
